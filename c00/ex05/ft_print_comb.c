a46503018c9706eadcd6e3aeda684c1e222024b9
jdias-mo <zmdmoreira@gmail.com>
2021-07-04 19:03:40 +0100

    ex05 review

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -2,23 +2,23 @@
 
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	char	a;
+	char	b;
+	char	c;
 
-	a = 47;
-	while (++a <= 57)
+	a = '0' - 1;
+	while (++a <= '7')
 	{
 		b = a;
-		while (++b <= 57)
+		while (++b <= '8')
 		{
 			c = b;
-			while (++c <= 57)
+			while (++c <= '9')
 			{
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if (a != 55)
+				if (a != '7')
 					write(1, ", ", 2);
 			}
 		}
